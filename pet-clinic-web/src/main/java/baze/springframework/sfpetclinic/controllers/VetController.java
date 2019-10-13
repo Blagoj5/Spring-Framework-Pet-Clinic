4238d5fe0fe1b0d4623fec07c342c829f27b2b1b
baze555 <bazeto25@gmail.com>
2019-10-13 02:56:46 +0200

    Added Vets List page and updated controller. closes #15

--- pet-clinic-web/src/main/java/baze/springframework/sfpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/baze/springframework/sfpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package baze.springframework.sfpetclinic.controllers;
 
+import baze.springframework.sfpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
 
