039443624eb98d18cef3adff87b67aa7b5e0fdbb
baze555 <bazeto25@gmail.com>
2019-10-21 02:45:43 +0200

    added Pet Type Map Service impl, closes #28

---
+++ pet-clinic-data/src/main/java/baze/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package baze.springframework.sfpetclinic.services;
+
+import baze.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
