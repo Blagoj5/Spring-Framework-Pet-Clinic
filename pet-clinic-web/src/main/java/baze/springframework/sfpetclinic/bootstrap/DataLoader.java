1c05eaabdbc9b96ed9c0929ed6d25ebcca2767ff
baze555 <bazeto25@gmail.com>
2019-10-21 14:16:19 +0200

    added PetTypes to bootstrap (DataLoader class), closes #31

--- pet-clinic-web/src/main/java/baze/springframework/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/baze/springframework/sfpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package baze.springframework.sfpetclinic.bootstrap;
 
 import baze.springframework.sfpetclinic.model.Owner;
+import baze.springframework.sfpetclinic.model.PetType;
 import baze.springframework.sfpetclinic.model.Vet;
 import baze.springframework.sfpetclinic.services.OwnerService;
+import baze.springframework.sfpetclinic.services.PetTypeService;
 import baze.springframework.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType petTypeSavedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType petTypeSavedCat = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
