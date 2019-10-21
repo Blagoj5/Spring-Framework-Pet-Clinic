9fbc92cc44745012b8dc46fc1cf36c1fcffcf6a4
baze555 <bazeto25@gmail.com>
2019-10-21 19:36:53 +0200

    added Speciality Map Service , closes #30

---
+++ pet-clinic-data/src/main/java/baze/springframework/sfpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package baze.springframework.sfpetclinic.services;
+
+import baze.springframework.sfpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
