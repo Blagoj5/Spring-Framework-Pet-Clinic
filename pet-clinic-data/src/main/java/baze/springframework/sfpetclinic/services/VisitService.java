23d2b8ea8de36a29a7a37b023823e6c00b301aa1
baze555 <bazeto25@gmail.com>
2019-10-30 15:19:22 +0100

    Added Visit map service, closes #45

---
+++ pet-clinic-data/src/main/java/baze/springframework/sfpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package baze.springframework.sfpetclinic.services;
+
+import baze.springframework.sfpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/baze/springframework/sfpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,47 @@
+package baze.springframework.sfpetclinic.services.map;
+
+import baze.springframework.sfpetclinic.model.Visit;
+import baze.springframework.sfpetclinic.services.PetService;
+import baze.springframework.sfpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final PetService petService;
+
+    public VisitMapService(PetService petService) {
+        this.petService = petService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getPetType() == null
+        || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
